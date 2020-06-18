057425f205f0321d35c70d2c5c085877eb75b047
20145208 <177315815@qq.com>
2020-06-18 23:19:47 +0800

    创建窗口和飞船

---
+++ settings.py
@@ -0,0 +1,10 @@
+# @Time  : 2020/6/18 0018 22:56
+# @Author: CaiYe
+# @File  : settings.py
+
+
+class Settings():
+    def __init__(self):
+        self.screen_width = 1200
+        self.screen_height = 600
+        self.bg_color = (230, 230, 230)
